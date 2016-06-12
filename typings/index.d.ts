c31a884e7c4bf29cbe971cfcd37b1c9a54b50273
Fahad Hossain <oneloopone@gmail.com>
2016-06-12 13:04:12 +0600

    Remove useless devendencies and expose React

--- typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="globals/history/index.d.ts" />
-/// <reference path="globals/lodash/index.d.ts" />
-/// <reference path="globals/material-ui/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/react-dom/index.d.ts" />
-/// <reference path="globals/react/index.d.ts" />
-/// <reference path="modules/react-router/index.d.ts" />
--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,11 @@ module.exports = {
     resolve: {
         extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
     },
-
     module: {
         loaders: [
-            { test: /\.tsx?$/, loader: "ts-loader" }
+            { test: /\.tsx?$/, loader: "ts-loader" },
+            { test: require.resolve("react"), loader: "expose?React" },
+            { test: require.resolve("react-dom"), loader: "expose?ReactDOM" }
         ],
         preLoaders: [
             { test: /\.js$/, loader: "source-map-loader" }
